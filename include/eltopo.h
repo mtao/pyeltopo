0139f47aad308a678259cfa7aa4354ac2cc21951
Michael Tao <mtao@dgp.toronto.edu>
2018-11-24 14:27:08 -0500

    initial commit with "working" code

---
+++ include/eltopo.h
@@ -0,0 +1,61 @@
+#pragma once
+
+
+//based off of ihttps://github.com/alecjacobson/gptoolbox/blob/master/mex/eltopo.cpp
+
+
+
+
+#include <eltopo3d/surftrack.h>
+#include <Eigen/Dense>
+#include <memory>
+
+class SurfTrack;
+
+class ElTopoTracker {
+    public:
+        using EigenVec3d = Eigen::Vector3d;
+        using EigenVec3i = Eigen::Vector3i;
+        using ColVectors3d = Eigen::Matrix<double,3,Eigen::Dynamic>;
+        using ColVectors3i = Eigen::Matrix<int,3,Eigen::Dynamic>;
+        using RefCV3d= Eigen::Ref<ColVectors3d>;
+        using RefCV3i = Eigen::Ref<ColVectors3i>;
+        using CRefCV3d= Eigen::Ref<const ColVectors3d>;
+        using CRefCV3i = Eigen::Ref<const ColVectors3i>;
+        ElTopoTracker& operator=(ElTopoTracker&&) = default;
+        ~ElTopoTracker();
+        ElTopoTracker(const CRefCV3d& V, const CRefCV3i& F, bool collision_safety=true, bool defrag_mesh = true, bool verbose = false);
+        static Eigen::VectorXd dual_volumes(const CRefCV3d& V, const CRefCV3i& F);
+
+        void split_edge(size_t edge_index);
+        //split_triangle splits by the longest edge
+        void split_triangle(size_t triangle_index);
+
+        void initialize();
+        void improve();
+        double integrate(const CRefCV3d& Vnew, double dt);
+        double step(const CRefCV3d& Vnew, double dt);
+        double integrate_py(const Eigen::Ref<const Eigen::MatrixXd>& Vnew, double dt) {
+            return integrate(Vnew,dt);
+        }
+        double step_py(const Eigen::Ref<const Eigen::MatrixXd>& Vnew, double dt) {
+            return step(Vnew,dt);
+        }
+
+        void defrag_mesh();
+
+        std::tuple<ColVectors3d,ColVectors3i> get_mesh() const;
+
+
+        SurfTrackInitializationParameters& init_params() { return m_init_params; }
+        ColVectors3d get_vertices() const;
+        ColVectors3i get_triangles() const;
+        SurfTrackInitializationParameters m_init_params;
+        bool m_verbose = false;
+        bool m_defrag_mesh = false;
+    private:
+        std::unique_ptr<SurfTrack> m_surf = nullptr;
+        std::unique_ptr<SubdivisionScheme> m_subdivision_scheme;
+        bool m_defrag_dirty = false;
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <memory>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+#include "eltopo.h"
+#include <iostream>
+
+#include <eltopo3d/eltopo.h>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(pyeltopo, m) {
+    py::class_<ElTopoTracker>(m, "ElTopoTracker")
+        //.def(py::init<const ElTopoTracker::CRefCV3d&, const ElTopoTracker::CRefCV3i&>())
+        .def(py::init<const ElTopoTracker::CRefCV3d&, const ElTopoTracker::CRefCV3i&,bool, bool, bool>(), py::arg("vertices"), py::arg("faces") ,py::arg("collision_safety")=true, py::arg("defrag_mesh")=true,py::arg("verbose") = false)
+        //.def(py::init<const ElTopoTracker::CRefCV3d&, const ElTopoTracker::CRefCV3i&,bool,bool>())
+        .def_readwrite("init_params", &ElTopoTracker::m_init_params)
+        .def_readwrite("enable_mesh_defrag", &ElTopoTracker::m_defrag_mesh)
+        .def("get_triangles",&ElTopoTracker::get_triangles)
+        .def("get_vertices",&ElTopoTracker::get_vertices)
+        .def("integrate",&ElTopoTracker::integrate_py)
+        .def("improve",&ElTopoTracker::improve)
+        .def("step",&ElTopoTracker::step_py)
+        .def("defrag_mesh",&ElTopoTracker::defrag_mesh)
+        .def("split_edge",&ElTopoTracker::split_edge)
+        .def("split_triangle",&ElTopoTracker::split_triangle);
+    py::class_<SurfTrackInitializationParameters>(m, "SurfTrackInitializationParameters")
+        .def(py::init<>())
+        .def_readwrite("proximity_epsilon",&SurfTrackInitializationParameters::m_proximity_epsilon)
+        .def_readwrite("friction_coefficient",&SurfTrackInitializationParameters::m_friction_coefficient)
+        .def_readwrite("min_triangle_area",&SurfTrackInitializationParameters::m_min_triangle_area)
+        .def_readwrite("improve_collision_epsilon",&SurfTrackInitializationParameters::m_improve_collision_epsilon)
+        .def_readwrite("use_fraction",&SurfTrackInitializationParameters::m_use_fraction)
+        .def_readwrite("min_edge_length",&SurfTrackInitializationParameters::m_min_edge_length)
+        .def_readwrite("max_edge_length",&SurfTrackInitializationParameters::m_max_edge_length) 
+        .def_readwrite("max_volume_change",&SurfTrackInitializationParameters::m_max_volume_change)
+        .def_readwrite("min_triangle_angle",&SurfTrackInitializationParameters::m_min_triangle_angle)
+        .def_readwrite("max_triangle_angle",&SurfTrackInitializationParameters::m_max_triangle_angle)   
+        .def_readwrite("use_curvature_when_splitting",&SurfTrackInitializationParameters::m_use_curvature_when_splitting)
+        .def_readwrite("use_curvature_when_collapsing",&SurfTrackInitializationParameters::m_use_curvature_when_collapsing)
+        .def_readwrite("min_curvature_multiplier",&SurfTrackInitializationParameters::m_min_curvature_multiplier)
+        .def_readwrite("max_curvature_multiplier",&SurfTrackInitializationParameters::m_max_curvature_multiplier)
+        .def_readwrite("allow_vertex_movement",&SurfTrackInitializationParameters::m_allow_vertex_movement)
+        .def_readwrite("edge_flip_min_length_change",&SurfTrackInitializationParameters::m_edge_flip_min_length_change)
+        .def_readwrite("merge_proximity_epsilon",&SurfTrackInitializationParameters::m_merge_proximity_epsilon)
+        .def_readwrite("collision_safety",&SurfTrackInitializationParameters::m_collision_safety)
+        .def_readwrite("allow_topology_changes",&SurfTrackInitializationParameters::m_allow_topology_changes)
+        .def_readwrite("allow_non_manifold",&SurfTrackInitializationParameters::m_allow_non_manifold)
+        .def_readwrite("perform_improvement",&SurfTrackInitializationParameters::m_perform_improvement);
+}
+
+
